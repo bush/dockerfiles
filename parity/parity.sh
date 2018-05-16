e26ce4ed46dce93f1709fdf3b7ac6712cbffc3dd
Dave Bush <davebush@gmail.com>
2018-05-16 11:56:48 +0000

    More parity cleanup
    Signed-off-by: Dave Bush <davebush@gmail.com>

--- parity/parity.sh
+++ parity/parity.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -it -v /home/docker/.local/share/io.parity.ethereum/docker/:/root/.local/share/io.parity.ethereum/ -p 8180:8180 -p 8545:8545 -p 8546:8546 -p 30303:30303 -p 30303:30303/udp parity/parity:v1.10.2 $@
+docker run -it -v /home/dave/stuff/parity/.local:/root/.local parity/parity:v1.10.2 $@
